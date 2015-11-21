52a06496f803dbaa41e5a0abbb28300048fafd1c
stefan judis <stefanjudis@gmail.com>
2015-11-21 21:57:25 +0100

    fix tests in extended reporter

--- reporters/extended.spec.js
+++ reporters/extended.spec.js
@@ -8,8 +8,16 @@ test( 'extended reporter', t => {
 
   t.same( result[ 0 ], 'Credits for foo' );
   t.same( result[ 1 ], 'foo relies on the work of 3 people:' );
-  t.same( result[ 3 ], 'Alice Bobson (3 packages) [ foo, bar, baz ]' );
-  t.same( result[ 4 ], 'Bob Calsow bob@calsow.io (2 packages) [ foo, bar ]' );
-  t.same( result[ 5 ], 'Jonny John (1 package) [ foo ]' );
+  t.same( result[ 3 ], 'Alice Bobson' );
+  t.same( result[ 4 ], '3 packages: foo, bar, baz' );
+  t.same( result[ 5 ], '' );
+  t.same( result[ 6 ], 'Bob Calsow' );
+  t.same( result[ 7 ], 'Mail: bob@calsow.io' );
+  t.same( result[ 8 ], '2 packages: foo, bar' );
+  t.same( result[ 9 ], '' );
+  t.same( result[ 10 ], 'Jonny John' );
+  t.same( result[ 11 ], 'GitHub: johnyjohn Twitter: @johnnyjohn' );
+  t.same( result[ 12 ], '1 package: foo' );
+
   t.end();
 } );
